2245c711ab816e5f1eda414f4b91a729be26927b
Marwa <marwa.maghnie@gmail.com>
2026-07-28 16:38:21 +0300

    gui: update app icon

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pathlib import Path
 # -------------------------------------------------------------------
 st.set_page_config(
     page_title="AI Shopping Assistant",
-    page_icon="static/app_icon.bmp",
+    page_icon="static/app_icon.png",
     layout="centered",
     initial_sidebar_state="collapsed"
 )
